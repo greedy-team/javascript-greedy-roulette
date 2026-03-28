a2ef7ceae2032ecd72a6b3092dd1f0c53d736165
rahwan10 <donggeon10@naver.com>
2026-03-28 17:56:02 +0900

    fix:사용자 실수 경고함수 개선및 이름변경

---
+++ src/views/hasValidationError.js
@@ -0,0 +1,45 @@
+export default hasValidationError
+
+function hasValidationError(BettingMoney,UserColor,UserAccount){
+    console.log("베팅머니:",BettingMoney);
+    if(!isNotInputNothing(BettingMoney)){
+        //alert("금액을 입력해주세요!");
+        return "금액을 입력해주세요!";
+    }
+    if (!isInputPossible(BettingMoney,UserAccount)) {
+        //alert("자금보다 작고 0보다 크게 설정해주세요!");
+        return "자금보다 작고 0보다 크게 설정해주세요!";
+    }
+    if (!UserColor) {
+        //alert("색상을 선택해주세요!");
+        return "색상을 선택해주세요!";
+    }
+    if (!isInt(BettingMoney,UserAccount)) {
+        //alert("정수를 적어주세요");
+        return "정수를 적어주세요";
+    }
+    if (String(BettingMoney).toLowerCase().includes('e')) {
+        //alert("지수 표기법으로 입력하지마세요");
+        return "지수 표기법으로 입력하지마세요";
+    }
+    return null;
+}
+
+function isInputPossible(BettingMoney,UserAccount) {
+    if (BettingMoney < 0 || BettingMoney > UserAccount) {
+        return false;
+    }
+    return true;
+}
+function isNotInputNothing(BettingMoney) {
+    if (BettingMoney=="") {
+        return false;
+    }
+    return true;
+}
+function isInt(BettingMoney) {
+    if (!(Number.isInteger(Number(BettingMoney)))) {
+        return false;
+    }
+    return true;
+}
